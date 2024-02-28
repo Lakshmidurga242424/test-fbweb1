1353e595aad3e4ac9c663c991c1f7b9eed4cb097
Lakshmidurga242424 <144435295+Lakshmidurga242424@users.noreply.github.com>
2024-02-28 12:24:41 +0530

    Update MainServlet.java

--- src/main/java/com/example/MainServlet.java
+++ src/main/java/com/example/MainServlet.java
@@ -22,7 +22,7 @@ import javax.servlet.http.Part;
 @MultipartConfig
 public class MainServlet extends HttpServlet {
     // JDBC connection parameters
-    private static final String JDBC_URL = "jdbc:mysql://192.168.56.151:3306/myDB";
+    private static final String JDBC_URL = "jdbc:mysql://192.168.56.151:3306/myDBB";
     private static final String JDBC_USER = "mysql";
     private static final String JDBC_PASSWORD = "mysql@123";
 
